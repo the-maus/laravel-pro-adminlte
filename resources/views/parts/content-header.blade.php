68156b5210c1c238de8f1697e8c6e2cfa677dc73
the-maus <msampaio.mail@gmail.com>
2025-10-24 01:19:07 -0300

    Initial template configuration

---
+++ resources/views/parts/content-header.blade.php
@@ -0,0 +1,22 @@
+<div class="app-content-header">
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-6">
+            @hasSection('page-title')
+                <h3 class="mb-0">@yield('page-title')</h3>
+            @endif
+
+            @isset($breadcrumbs)
+            <ol class="breadcrumb">
+                @foreach ($breadcrumbs as $crumb)
+                    <li class="breadcrumb-item"><a href="{{ $crumb['route'] }}">{{ $crumb['label'] }}</a></li>
+                @endforeach
+            </ol>
+            @endisset
+        </div>
+        <div class="col-sm-6">
+            Actions
+        </div>
+    </div>
+    </div>
+</div>
