dffaa28098ec546fb2abf44a70b60107f87db89c
the-maus <msampaio.mail@gmail.com>
2025-10-27 17:42:33 -0300

    Forgot password option

--- resources/views/auth/forgot_password.blade.php
+++ resources/views/auth/forgot_password.blade.php
@@ -1,1 +1,35 @@
-Forgot Password
+@extends('layouts.auth')
+
+@section('body-class', 'login-page')
+
+@section('content')
+<div class="login-box">
+    <div class="login-logo">
+    <a href="../index2.html"><b>Admin</b>LTE</a>
+    </div>
+    <div class="card">
+    <div class="card-body login-card-body">
+        <p class="login-box-msg">Sign in to start your session</p>
+        <form action="{{ route('password.email') }}" method="post">
+        @csrf
+        <div class="input-group mb-3">
+            <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+            <input type="email" name="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" placeholder="Email" />
+            @error('email')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-primary">Send me the link</button>
+        </div>
+        </form>
+        <div class="mt-2 text-center">
+            <p class="mb-0">
+            <a href="register.html" class="text-center">Back to login</a>
+            </p>
+        </div>
+    </div>
+    <!-- /.login-card-body -->
+    </div>
+</div>
+@endsection
--- resources/views/auth/reset_password.blade.php
+++ resources/views/auth/reset_password.blade.php
@@ -1,1 +1,49 @@
-Reset Password
+@extends('layouts.auth')
+
+@section('body-class', 'register-page')
+
+@section('content')
+<div class="register-box">
+    <div class="register-logo">
+    <a href="../index2.html"><b>Admin</b>LTE</a>
+    </div>
+    <div class="card">
+    <div class="card-body register-card-body">
+        {{-- @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <div>
+                    {{ $error }}
+                </div>
+            @endforeach
+        @endif --}}
+
+        <p class="register-box-msg">Register a new membership</p>
+        <form action="{{ route('password.update') }}" method="post">
+            @csrf
+            <input type="hidden" name="token" value="{{ request()->token }}">
+            <div class="input-group mb-3">
+                <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ request()->email }}"/>
+                @error('email')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" />
+                @error('password')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                <input type="password" name="password_confirmation" class="form-control" placeholder="Password Confirmation" />
+            </div>
+            <div class="d-grid gap-2">
+                <button type="submit" class="btn btn-primary">Reset password</button>
+            </div>
+        </form>
+    </div>
+    </div>
+</div>
+@endsection
